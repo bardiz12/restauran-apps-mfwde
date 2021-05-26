cac651d15dad5fcbf9a405bf49fa87aa4707205f
Bardizba <dizba.seller@gmail.com>
2021-05-26 10:27:01 +0700

    fixing drawer bug and overflow text review

--- src/scripts/DrawerInitiator.js
+++ src/scripts/DrawerInitiator.js
@@ -5,6 +5,11 @@ const DrawerInitiator = {
         this.drawer = drawer;
         this.content = content;
         button.addEventListener('click', (event) => this.toggleDrawer(event));
+        this.drawer.addEventListener('click', (event) => {
+            if (event.target.nodeName === "A") {
+                this.hideDrawer()
+            }
+        })
     },
 
     toggleDrawer() {
@@ -13,6 +18,13 @@ const DrawerInitiator = {
         this.drawer.classList.toggle('mobile-nav-active');
         this.content.classList.toggle('disable-scroll');
     },
+
+    hideDrawer() {
+        const toggler = this.button.querySelector('span');
+        toggler.classList.remove('active');
+        this.drawer.classList.remove('mobile-nav-active');
+        this.content.classList.remove('disable-scroll');
+    }
 };
 
 export default DrawerInitiator;
